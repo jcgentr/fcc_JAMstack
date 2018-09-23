d472fe39aed8804f2808253b312c710f228a5bc9
Jared Gentry <jcgentr@clemson.edu>
2018-09-23 11:48:15 -0400

    add alt attribute to img

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => (
     <h1>Hello my name is Jared Gentry!</h1>
     <p>This is my first Gatsby site.</p>
     <p>I am ready to build something great.</p>
-    <img src="https://media2.giphy.com/media/4kd7EL6lWaW9W/giphy.webp"></img>
+    <img src="https://media2.giphy.com/media/4kd7EL6lWaW9W/giphy.webp" alt="Bill Murray basketball assistance"></img>
     <br></br>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
